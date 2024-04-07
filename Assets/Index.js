3e33f1be1f711e1dc0b8d2e03fd0939b8cfbc1dc
Louis Richard <luisrichard2580@gmail.com>
2024-04-07 09:59:35 +0300

    Complete The Project

---
+++ Assets/Index.js
@@ -0,0 +1,35 @@
+function likeFunction(){
+
+    var like = document.getElementById('like');
+    like.style.fontWeight = "bold";
+    like.style.backgroundColor = "Blue";
+    like.style.color = "White";
+  
+    like.innerHTML = "&#10003; Liked";
+}
+
+
+// function fun(){
+
+//     var like = document.getElementById('like');
+//     like.style.fontWeight = "bold";
+//     like.style.backgroundColor = "Blue";
+//     like.style.color = "White";
+  
+//    // like.innerHTML = "&#10003; Liked";
+// }
+// function fun() {
+//     x.style.fontWeight = "bold";
+//     x.style.backgroundColor = "Blue";
+//     x.style.color = "White"
+//     //x.innerHTML = "&#10003; Liked";
+// }
+
+// function unlikeFunction(x) {
+//   let x = likeFunction === false;
+//   if(x === false){
+//     x.style.fontWeight = "bold";
+//     x.style.backgroundColor = "white";
+//     x.style.color = "black"
+//   }
+// }
